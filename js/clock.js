3d748f4cdd6698dcc112daac08c00c96c1e204ea
Mars Cheung <marscheung@Marss-MacBook-Pro.local>
2017-06-11 21:27:24 -0700

    calibrate clocks for all four timezones

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,39 @@
-
-function runMountainClock(){
+function calculateUTCTime(){
 	const date = new Date();
 	const localTime = date.getTime();
 	const localOffset = date.getTimezoneOffset() * 60000;
 	const utc = localTime + localOffset;
+	return utc;
+}
+
+
+function runPacificClock(){
+	const utc = calculateUTCTime();
+	const pacificTime = utc - (3600000*7);
+	const pacificDate = new Date(pacificTime);
+
+	let pacificSeconds = pacificDate.getSeconds();
+	let pacificMinutes = pacificDate.getMinutes();
+	let pacificHours = pacificDate.getHours();
+
+	pacificHours = (pacificHours > 12) ? (pacificHours - 12) : pacificHours;
+	let secondsDegrees = (pacificSeconds * 6) + 90;
+	let minutesDegrees = (pacificMinutes * 6) + 90;
+	let hoursDegrees = (pacificHours * 30) + 90;
+
+	let secondHand = document.querySelector(`.hand-second-pacific`);
+	let minuteHand = document.querySelector(`.hand-minute-pacific`);
+	let hourHand = document.querySelector(`.hand-hour-pacific`);
+
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+	minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+	console.log('Pacific Time: ' + utc + ' ' + pacificDate);
+}
+
+
+function runMountainClock(){
+	const utc = calculateUTCTime();
 	const mountainTime = utc - (3600000*6);
 	const mountainDate = new Date(mountainTime); 
 
@@ -27,10 +57,7 @@ function runMountainClock(){
 }
 
 function runCentralClock(){
-	const date = new Date();
-	const localTime = date.getTime();
-	const localOffset = date.getTimezoneOffset() * 60000;
-	const utc = localTime + localOffset;
+	const utc = calculateUTCTime();
 	const centralTime = utc - (3600000*5);
 	const centralDate = new Date(centralTime);
 
@@ -53,9 +80,34 @@ function runCentralClock(){
 	console.log('Central Time: ' + centralDate);
 }
 
+function runEasternClock(){
+	const utc = calculateUTCTime();
+	const easternTime = utc - (3600000*4);
+	const easternDate = new Date(easternTime);
+
+	let seconds = easternDate.getSeconds();
+	let minutes = easternDate.getMinutes();
+	let hours = easternDate.getHours();
+
+	let secondsDegrees = (seconds * 6) + 90;
+	let minutesDegrees = (minutes * 6) + 90;
+	let hoursDegrees = (hours * 30) + 90;
+
+	let secondHand = document.querySelector(`.hand-second-eastern`);
+	let minuteHand = document.querySelector(`.hand-minute-eastern`);
+	let hourHand = document.querySelector(`.hand-hour-eastern`);
+
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+	minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+	console.log('Eastern Time: ' + easternDate);
+}
+
 function animateClocks(){
+	runPacificClock();
 	runMountainClock();
 	runCentralClock();
+	runEasternClock();
 }
 
 setInterval(animateClocks, 1000);
